319b8cbe0941e0ede4d1c51aea3636949c107a43
typicalaimster <evol@zwei.net>
2024-07-01 15:26:51 -0700

    Somechanges (#2)
    * gatekeeping
    
    * update configinit

--- docker/configini.sh
+++ docker/configini.sh
@@ -3,5 +3,5 @@ if [ ! -f "/config/config.ini" ]; then
     cp "config.ini" "config/config.ini"
 fi
 if [ ! -f "config/fortunes.txt" ]; then
-    cp "fortunes.txt" "/config/fortunes.txt"
+    cp "fortunes.txt" "config/fortunes.txt"
 fi
